d9500236b8f9f5c47a07c8fcb3dd147b502e11ef
mcepulis <109752570+mcepulis@users.noreply.github.com>
2025-04-15 15:06:59 +0300

    isLoggedIn functionality from backend side

--- backend/app/login.py
+++ backend/app/login.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 from .database import get_db
 import bcrypt
+from datetime import timedelta
+from .auth import create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
 
 router = APIRouter()
 
@@ -11,4 +13,18 @@ def login_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     existing_user = db.query(models.User).filter(models.User.username == user.username).first()
     if not existing_user or not bcrypt.checkpw(user.password.encode('utf-8'), existing_user.password_hash.encode('utf-8')):
         raise HTTPException(status_code=401, detail="Invalid credentials")
-    return f'User {user.username} logged in successfully'
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+    data={
+        "sub": existing_user.username,
+        "user_id": str(existing_user.id), 
+    },
+    expires_delta=access_token_expires
+)
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "expires_in": ACCESS_TOKEN_EXPIRE_MINUTES * 60  
+    }
